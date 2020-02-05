e90568fe2a8fcaf03720fe91b44b00507a8d0091
Ryan Zagala <gman10501@gmail.com>
2020-02-04 17:02:07 -0800

    Deleted old lab2 folder.

--- lab2/testmux.v
+++
@@ -1,18 +0,0 @@
-%PDF-1.5
-%ø˜¢˛
-37 0 obj
-<< /Linearized 1 /L 214882 /H [ 845 225 ] /O 41 /E 69983 /N 15 /T 214391 >>
-endobj
-                                                                                                            
-38 0 obj
-<< /Type /XRef /Length 65 /Filter /FlateDecode /DecodeParms << /Columns 5 /Predictor 12 >> /W [ 1 3 1 ] /Index [ 37 37 ] /Info 49 0 R /Root 39 0 R /Size 74 /Prev 214392                /ID [<80a13a6268463e734e0a07942d13411e><80a13a6268463e734e0a07942d13411e>] >>
-stream
-xúcbd`‡g`b``8	"ôù¿lc…¯L≤ÉH5Q…ö"vÄ≈;Ä$cFòù…¿ƒ¯ˇ[9X/„‡!û#ï
-endstream
-endobj
-                                                                                                                                                                                                                        
-39 0 obj
-<< /Pages 65 0 R /Type /Catalog >>
-endobj
-40 0 obj
-<< /Filter /FlateDecode /S 155 /Le
